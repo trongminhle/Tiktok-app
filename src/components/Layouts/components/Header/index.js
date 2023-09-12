e54a5f4cf1ed02ac943de993c791e81ae0988d47
trongminhle <leminhtrong25012000@gmail.com>
2023-09-12 15:28:42 +0700

    Xây dựng UI phần Header #3

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+   faCircleXmark,
+   faSpinner,
+   faMagnifyingGlass,
+   faSignIn,
+} from "@fortawesome/free-solid-svg-icons";
 import Tippy from "@tippyjs/react/headless";
 
+import Button from "~/components/Button";
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
 import { Wrapper as PopperWrapper } from "~/components/Popper";
@@ -16,7 +22,7 @@ function Header() {
 
    useEffect(() => {
       setTimeout(() => {
-         setSearchResult([1, 2, 3]);
+         setSearchResult([]);
       }, 0);
    }, []);
 
@@ -53,7 +59,10 @@ function Header() {
                   </button>
                </div>
             </Tippy>
-            <div className={cx("action")}></div>
+            <div className={cx("action")}>
+               <Button text>Upload</Button>
+               <Button primary>Log in</Button>
+            </div>
          </div>
       </header>
    );
